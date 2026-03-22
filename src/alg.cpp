4d10678e5efb19f99ec6657321b93f4bfebf9cce
pkonysheva <pkonyseva4@gmail.com>
2026-03-22 14:37:44 +0300

    Конышева 25-ИСТ-1-1 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,13 @@
 uint64_t collatzMaxValue(uint64_t num) {
     uint64_t max = num;
     while (num != 1) {
-        if (num % 2 == 0) num /= 2;
-        else num = 3 * num + 1;
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = 3 * num + 1;
 
-        if (num > max) max = num;
+        if (num > max)
+            max = num;
     }
     return max;
 }
@@ -16,8 +19,10 @@ uint64_t collatzMaxValue(uint64_t num) {
 unsigned int collatzLen(uint64_t num) {
     unsigned int len = 1;
     while (num != 1) {
-        if (num % 2 == 0) num /= 2;
-        else num = 3 * num + 1;
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = 3 * num + 1;
         len++;
     }
     return len;
@@ -29,6 +34,7 @@ unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) 
 
     for (uint64_t i = lbound; i <= rbound; i++) {
         unsigned int len = collatzLen(i);
+
         if (len > resmax) {
             resmax = len;
             num = i;
